5b68371d6a0b54bd4256de045f760e1842c7f33a
Victor <annimon119@gmail.com>
2017-09-02 11:04:26 +0300

    Add shape properties

--- app/src/main/java/com/annimon/hotarufx/visual/objects/ShapeNode.java
+++ app/src/main/java/com/annimon/hotarufx/visual/objects/ShapeNode.java
@@ -12,13 +12,23 @@ public abstract class ShapeNode extends ObjectNode {
 
     private final Shape shape;
 
+    private PropertyTimelineHolder<Boolean> smooth;
     private PropertyTimelineHolder<Paint> fill, stroke;
+    private PropertyTimelineHolder<Number> strokeWidth, strokeDashOffset, strokeMiterLimit;
 
     public ShapeNode(Shape shape) {
         super(shape);
         this.shape = shape;
+        smooth = PropertyTimelineHolder.empty();
         fill = PropertyTimelineHolder.empty();
         stroke = PropertyTimelineHolder.empty();
+        strokeWidth = PropertyTimelineHolder.empty();
+        strokeDashOffset = PropertyTimelineHolder.empty();
+        strokeMiterLimit = PropertyTimelineHolder.empty();
+    }
+
+    public PropertyTimeline<Boolean> smoothProperty() {
+        return smooth.setIfEmptyThenGet(shape::smoothProperty);
     }
 
     public PropertyTimeline<Paint> fillProperty() {
@@ -29,17 +39,37 @@ public abstract class ShapeNode extends ObjectNode {
         return stroke.setIfEmptyThenGet(shape::strokeProperty);
     }
 
+    public PropertyTimeline<Number> strokeWidthProperty() {
+        return strokeWidth.setIfEmptyThenGet(shape::strokeWidthProperty);
+    }
+
+    public PropertyTimeline<Number> strokeDashOffsetProperty() {
+        return strokeDashOffset.setIfEmptyThenGet(shape::strokeDashOffsetProperty);
+    }
+
+    public PropertyTimeline<Number> strokeMiterLimitProperty() {
+        return strokeMiterLimit.setIfEmptyThenGet(shape::strokeMiterLimitProperty);
+    }
+
     @Override
     public void buildTimeline(TimeLine timeline) {
         super.buildTimeline(timeline);
+        smooth.applyIfPresent(timeline);
         fill.applyIfPresent(timeline);
         stroke.applyIfPresent(timeline);
+        strokeWidth.applyIfPresent(timeline);
+        strokeDashOffset.applyIfPresent(timeline);
+        strokeMiterLimit.applyIfPresent(timeline);
     }
 
     @Override
     public PropertyBindings propertyBindings(PropertyBindings bindings) {
         return super.propertyBindings(bindings)
+                .add("smooth", BOOLEAN, this::smoothProperty)
                 .add("fill", PAINT, this::fillProperty)
-                .add("stroke", PAINT, this::strokeProperty);
+                .add("stroke", PAINT, this::strokeProperty)
+                .add("strokeWidth", NUMBER, this::strokeWidthProperty)
+                .add("strokeDashOffset", NUMBER, this::strokeDashOffsetProperty)
+                .add("strokeMiterLimit", NUMBER, this::strokeMiterLimitProperty);
     }
 }
