306f3efc329e3149a8ee70694a165d8ea63f6ad4
Victor <annimon119@gmail.com>
2017-09-04 15:14:42 +0300

    Add editor

--- app/src/main/java/com/annimon/hotarufx/Main.java
+++ app/src/main/java/com/annimon/hotarufx/Main.java
@@ -1,17 +1,9 @@
 package com.annimon.hotarufx;
 
-import com.annimon.hotarufx.bundles.BundleLoader;
-import com.annimon.hotarufx.bundles.CompositionBundle;
-import com.annimon.hotarufx.bundles.NodesBundle;
-import com.annimon.hotarufx.lexer.HotaruLexer;
-import com.annimon.hotarufx.lib.Context;
-import com.annimon.hotarufx.parser.HotaruParser;
-import com.annimon.hotarufx.parser.visitors.InterpreterVisitor;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.val;
 
@@ -19,43 +11,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        val input = readProgram("/main.hfx");
-        val context = new Context();
-        BundleLoader.load(context, Arrays.asList(
-                CompositionBundle.class,
-                NodesBundle.class
-        ));
-
-        HotaruParser.parse(HotaruLexer.tokenize(input))
-                .accept(new InterpreterVisitor(), context);
-
-        val composition = context.composition();
         primaryStage.setTitle("HotaruFX");
-        primaryStage.setScene(composition.produceAnimationScene());
-        composition.getTimeline().getFxTimeline().play();
+        try {
+            val loader = new FXMLLoader(getClass().getResource("/fxml/Editor.fxml"));
+            val scene = new Scene(loader.load());
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            // TODO: notice me!!
+        }
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    private static String readProgram(String path) {
-        val fallbackProgram = "composition(640, 480, 25)";
-        try (InputStream is = Main.class.getResourceAsStream(path)) {
-            if (is == null) {
-                return fallbackProgram;
-            }
-            val baos = new ByteArrayOutputStream();
-            val bufferSize = 4096;
-            val buffer = new byte[bufferSize];
-            int read;
-            while ((read = is.read(buffer, 0, bufferSize)) != -1)  {
-                baos.write(buffer, 0, read);
-            }
-            return baos.toString("UTF-8");
-        } catch (IOException ioe) {
-            return fallbackProgram;
-        }
     }
 }
---
+++ app/src/main/java/com/annimon/hotarufx/ui/controller/EditorController.java
@@ -0,0 +1,99 @@
+package com.annimon.hotarufx.ui.controller;
+
+import com.annimon.hotarufx.Main;
+import com.annimon.hotarufx.bundles.BundleLoader;
+import com.annimon.hotarufx.bundles.CompositionBundle;
+import com.annimon.hotarufx.bundles.NodesBundle;
+import com.annimon.hotarufx.lexer.HotaruLexer;
+import com.annimon.hotarufx.lib.Context;
+import com.annimon.hotarufx.parser.HotaruParser;
+import com.annimon.hotarufx.parser.ParseError;
+import com.annimon.hotarufx.parser.visitors.InterpreterVisitor;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
+import lombok.val;
+
+public class EditorController implements Initializable {
+
+    @FXML
+    public TextArea editor;
+
+    @FXML
+    public TextArea log;
+
+    @FXML
+    public void handleMenuExit(ActionEvent event) {
+        // TODO: confirmation
+        Platform.exit();
+    }
+
+    @FXML
+    public void handleMenuAbout(ActionEvent event) {
+    }
+
+    @FXML
+    public void handleMenuPlay(ActionEvent event) {
+        log.setText("");
+        val input = editor.getText();
+
+        val context = new Context();
+        BundleLoader.load(context, Arrays.asList(
+                CompositionBundle.class,
+                NodesBundle.class
+        ));
+
+        val parser = new HotaruParser(HotaruLexer.tokenize(input));
+        val program = parser.parse();
+        if (parser.getParseErrors().hasErrors()) {
+            val sb = new StringBuilder();
+            for (ParseError parseError : parser.getParseErrors()) {
+                sb.append(parseError);
+            }
+            log.setText(sb.toString());
+            return;
+        }
+        program.accept(new InterpreterVisitor(), context);
+
+        val stage = new Stage();
+        val composition = context.composition();
+        stage.setScene(composition.produceAnimationScene());
+        composition.getTimeline().getFxTimeline().play();
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        editor.setText(readProgram("/main.hfx"));
+    }
+
+    private static String readProgram(String path) {
+        val fallbackProgram = "composition(640, 480, 25)";
+        try (InputStream is = Main.class.getResourceAsStream(path)) {
+            if (is == null) {
+                return fallbackProgram;
+            }
+            val baos = new ByteArrayOutputStream();
+            val bufferSize = 4096;
+            val buffer = new byte[bufferSize];
+            int read;
+            while ((read = is.read(buffer, 0, bufferSize)) != -1)  {
+                baos.write(buffer, 0, read);
+            }
+            return baos.toString("UTF-8");
+        } catch (IOException ioe) {
+            return fallbackProgram;
+        }
+    }
+}
